71f3bca17af48fc4433c42469c1fe99ab2562394
dpurhar27 <devan.p@shaw.ca>
2018-11-08 12:55:00 -0800

    Added ability for any data set to be used in Plotter()'s

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
     plotter = Plotter()
 
     for i in features.columns :
-        plotter.visualizeBarChart(i, "X label", "Y label")
-        plotter.visualizeScatterPlot(i, "X label", "Y label", labels)
-        plotter.visualizeHistogram(i, "X Label", "Y Label")
-        # plotter.visualizeLine(range(70000), i, "X Label", "Y Label")
+        plotter.visualize_bar_chart(i, "X label", "Y label")
+        plotter.visualize_scatter_plot(i, "X label", "Y label", labels)
+        plotter.visualize_histogram(i, "X Label", "Y Label")
+        # plotter.visualize_line(range(70000), i, "X Label", "Y Label")
 
--- plotter.py
+++ plotter.py
@@ -12,78 +12,103 @@ Featured charts are: Histogram, Bar Chart, Scatter Plot, Line Plot
 Version: 1.0.0
 
 """
+
+
 class Plotter:
 
     def __init__(self):
         self.data = DataSet()
 
-    def visualizeBarChart(self, featureName, XLabel, YLabel) :
+    def visualize_bar_chart(self, feature_name, x_label, y_label, data=None) :
         """
 
         Create a bar chart of the specified feature.
 
-        :param featureName: Name of the feature to be visualized
-        :param XLabel: Label for X axis
-        :param YLabel: Label for Y axis
+        :param feature_name: Name of the feature to be visualized
+        :param x_label: Label for X axis
+        :param y_label: Label for Y axis
+        :param data: The dataset, defaults to null so sets to overall dataset
         :return: Bar Chart
         """
-        feature, counts = np.unique(self.data.get_trainX_pd().loc[:, featureName], return_counts=True)
+
+        if data:
+            feature, counts = np.unique(data.get_trainX_pd().loc[:, feature_name], return_counts=True)
+        else:
+            feature, counts = np.unique(self.data.get_trainX_pd().loc[:, feature_name], return_counts=True)
         plt.bar(feature, counts)
-        plt.xlabel(XLabel)
-        plt.ylabel(YLabel)
-        plt.title(featureName + " Bar Chart")
+        plt.x_label(x_label)
+        plt.y_label(y_label)
+        plt.title(feature_name + " Bar Chart")
         plt.show()
 
-    def visualizeScatterPlot(self, featureName, XLabel, YLabel, labelName) :
+    def visualize_scatter_plot(self, feature_name, x_label, y_label, label_name, data=None) :
         """
 
         Create a scatter plot of the specified feature.
 
-        :param featureName: Name of the feature to be visualized
-        :param XLabel: Label for X axis
-        :param YLabel: Label for Y axis
+        :param feature_name: Name of the feature to be visualized
+        :param x_label: Label for X axis
+        :param y_label: Label for Y axis
+        :param: label_name: Label name
         :return: Scatter Plot
         """
-
-        feature = self.data.get_trainX_pd().loc[:, featureName]
-        plt.scatter(feature, labelName)
-        plt.xlabel(XLabel)
-        plt.ylabel(YLabel)
-        plt.title(featureName + " Scatter Plot")
+        if data:
+            feature = data.get_trainX_pd().loc[:, feature_name]
+        else:
+            feature = self.data.get_trainX_pd().loc[:, feature_name]
+        plt.scatter(feature, label_name)
+        plt.x_label(x_label)
+        plt.y_label(y_label)
+        plt.title(feature_name + " Scatter Plot")
         plt.show()
 
-    def visualizeHistogram(self, featureName, XLabel, YLabel, binNum=10) :
+    def visualize_histogram(self, feature_name, x_label, y_label, data=None, bin_num=10) :
         """
 
         Create a Histogram of the specified feature.
 
-        :param featureName: Name of the feature to be visualized
-        :param XLabel: Label for X axis
-        :param YLabel: Label for Y axis
+        :param feature_name: Name of the feature to be visualized
+        :param x_label: Label for X axis
+        :param y_label: Label for Y axis
+        :param data: Dataset
         :param binNum: Number of bins
         :return: Histogram
         """
-
-        plt.hist(self.data.get_trainX_pd().loc[:, featureName], bins=binNum)
-        plt.xlabel(XLabel)
-        plt.ylabel(YLabel)
-        plt.title(featureName)
+        if data:
+            plt.hist(data.get_trainX_pd().loc[:, feature_name], bins=bin_num)
+        else:
+            plt.hist(self.data.get_trainX_pd().loc[:, feature_name], bins=bin_num)
+        plt.x_label(x_label)
+        plt.y_label(y_label)
+        plt.title(feature_name)
         plt.show()
 
-    def visualizeLine(self, values, featureName, XLabel, YLabel):
+    def visualize_line(self, values, feature_name, x_label, y_label, data=None):
         """
 
         Create a line plot
 
         :param values: The range of the values on the Y axis ( I think )
-        :param plotValue: The values to plot as a line
-        :param XLabel: Label for X axis
-        :param YLabel: Label for Y axis
+        :param feature_name: The values to plot as a line
+        :param x_label: Label for X axis
+        :param y_label: Label for Y axis
+        :param data: Dataset , default null so overall dataset is used
         :return: Line Plot
         """
-
-        plt.plot(values, self.data.get_trainX_pd().loc[:, featureName])
-        plt.xlabel(XLabel)
-        plt.ylabel(YLabel)
+        if data:
+            plt.plot(values, data.get_trainX_pd().loc[:, feature_name])
+        else:
+            plt.plot(values, self.data.get_trainX_pd().loc[:, feature_name])
+        plt.x_label(x_label)
+        plt.y_label(y_label)
         plt.title("Line Plot")
         plt.show()
+
+
+    # def visualizeData(self, type, feature, label, x_label, y_label):
+    #
+    #     if type == "bar":
+    #         return self.visualizeBarChart(feature, x_label, y_label)
+    #     elif type == "hist":
+    #         return self.visualizeHistogram(feature, x_label, y_label)
+    #     return self.visualizeScatterPlot(feature, x_label, y_label, label)
