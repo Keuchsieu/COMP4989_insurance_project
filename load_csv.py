2afedb609339f621e6a870a0f79261e687d5f358
dpurhar27 <devan.p@shaw.ca>
2018-11-08 19:55:20 -0800

    added claimX/Y and no_claimX/Y + getters

--- load_csv.py
+++ load_csv.py
@@ -9,12 +9,12 @@ class DataSet:
         self.trainingX = self.training.drop(['rowIndex', 'ClaimAmount'], 1)  # dropping label column and index
         self.trainingY = self.training['ClaimAmount']  # getting only label column
         self.testingX = testset.drop(['rowIndex'], 1)  # drop row index
-        self.no_claims = self.training[self.training['ClaimAmount'] == 0]
-        self.claims = self.training[self.training['ClaimAmount'] != 0]
-        self.no_claim_X = 0
-        self.no_claim_Y = 0
-        self.claim_X = 0
-        self.claim_Y = 0
+        self.no_claims = self.training[self.training['ClaimAmount'] == 0] # get the samples with no claims
+        self.claims = self.training[self.training['ClaimAmount'] != 0] # get the samples with claims
+        self.no_claim_X = self.no_claims.drop(['rowIndex', 'ClaimAmount'], 1) # repeat above for this data-set
+        self.no_claim_Y = self.no_claims['ClaimAmount']
+        self.claim_X = self.claims.drop(['rowIndex', 'ClaimAmount'], 1)
+        self.claim_Y = self.claims['ClaimAmount']
 
     def get_trainX(self):
         return np.array(self.trainingX)
@@ -33,6 +33,30 @@ class DataSet:
 
     def get_testX_pd(self):
         return self.testingX
+
+    def get_noClaimX_pd(self):
+        return self.no_claim_X
+
+    def get_noClaimY_pd(self):
+        return self.no_claim_Y
+
+    def get_claimX_pd(self):
+        return self.claim_X
+
+    def get_claimY_pd(self):
+        return self.claim_Y
+
+    def get_noClaimX(self):
+        return np.array(self.get_noClaimX_pd())
+
+    def get_noClaimY(self):
+        return np.array(self.get_noClaimX_pd())
+
+    def get_claimX(self):
+        return np.array(self.get_claimX_pd())
+
+    def get_claimY(self):
+        return np.array(self.get_claimY_pd())
 
 
 if __name__ == '__main__':
