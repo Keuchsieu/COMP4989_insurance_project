cb3285ff4f9f6f3beb70b6bc1c0bc5f51ab5eeaa
yuqingqiu <yuqingqiu93@gmail.com>
2018-11-15 10:08:41 -0800

    Kfold calls error callculation based on argument method

--- trainingModel.py
+++ trainingModel.py
@@ -24,15 +24,16 @@ def calculate_error(y, yhat, method='mae'):
     if method == 'mae':
         mae = np.mean(np.abs(y - yhat))
         return mae
+    elif method == 'knn':
+        y_local = y
+        for i in range(len(y)):
+            if y[i] > 0:
+                y_local[i] = 1
+            else:
+                y_local[i] = int(y[i])
+        return np.mean(np.abs(y_local-yhat))
+    # return none if the method is not specified above
     return None
-
-def calculate_error_knn(y, yhat):
-    for i in range(len(y)):
-        if y[i] > 0:
-            y[i] = 1
-        else:
-            y[i] = int(y[i])
-    return np.mean(np.abs(y-yhat))
 
 
 def set_model(model_type):
@@ -41,7 +42,8 @@ def set_model(model_type):
     elif model_type == "Ridge":
         return Ridge()
     elif model_type == "kNN":
-        return modified_knn()
+        pass  # the function is not a model, it returns prediction
+        # return modified_knn()
     return Lasso()
 
 
@@ -78,6 +80,7 @@ def k_fold(x, y, K, func=None, model=None, **kwargs):
     chunk = int(data_length / K)
     min_error = -1
     best_k = 0
+    errors = []
     for i in range(K):
         x_cv = []  # one chunk size of x
         y_cv = []
@@ -90,17 +93,18 @@ def k_fold(x, y, K, func=None, model=None, **kwargs):
             else:
                 Xtrain.append(x[j])
                 Ytrain.append(y[j])
-        #if func:
-        y_hat = modified_knn(x_cv, Xtrain, Ytrain, **kwargs)
-        #else:
-            #model.fit(Xtrain, Ytrain)
-            #y_hat = model.predict(x_cv)
-        #error = calculate_error(y_cv, y_hat)
-        error = calculate_error_knn(y_cv, y_hat)
+        if func:  # if predicting using a written function
+            y_hat = func(x_cv, Xtrain, Ytrain, **kwargs)
+        else:  # if training a model from sklearn
+            model.fit(Xtrain, Ytrain)
+            y_hat = model.predict(x_cv)
+        error = calculate_error(y_cv, y_hat, **kwargs)
+        errors.append(error)
         if min_error == -1 or error < min_error:
             min_error = error
             best_k = i
-    return best_k, min_error
+    average_error = np.mean(errors)
+    return best_k, min_error, average_error
 
 
 if __name__ == '__main__':
@@ -111,5 +115,5 @@ if __name__ == '__main__':
     #bk, me = k_fold(data.get_testX(), data.get_trainY(), K=10, model=model)
     #print(bk, me)
 
-    bk, me = k_fold(data.get_testX(), data.get_trainY(), K=10)
-    print(bk, me)
+    bk, me, average = k_fold(data.get_trainX(), data.get_trainY(), K=10, func=modified_knn, method='knn')
+    print("Best K fold group number {}, min error: {}, average error {}".format(bk, me, average))
