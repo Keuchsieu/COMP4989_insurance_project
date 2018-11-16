1d4e915aeda69b1266625dde6eeb1e0aae08bfdf
Miranda Pattyn <miranda.pattyn@gmail.com>
2018-11-15 19:46:20 -0800

    Reformated file to include method returning sorted array of features and coefficients

--- ridgeLassoCoef.py
+++ ridgeLassoCoef.py
@@ -1,62 +1,38 @@
-from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
 import numpy as np
 import preprocessor as pre
 
-def calculate_error(y, yhat, method='mae'):
-    if method == 'mae':
-        mae = np.mean(np.abs(y - yhat))
-        return mae
-    return None
-
 
 def set_model(model_type):
-    if model_type == 'Linear':
-        return LinearRegression()
-    elif model_type == "Ridge":
+    if model_type == "Ridge":
         return Ridge()
     return Lasso()
 
 
-def k_fold(x, y, K, func=None, model=None, **kwargs):
+def k_fold_get_coefs(x, y, K, features, model=None):
     """
     :param x: Feature matrix, type of ndarray
     :param y: Label Vector, type of ndarray
     :param K: number of K fold
-    :param func: the function that runs the validation training and predicting
-    :param model: a model from sklearn that fit() and predict()
-    :param kwargs: arguments that functions differently for different model
-    :return: the error rate of the data
+    :param model: a model from sklearn that fit() and predict(), either ridge or lasso
+    :return: Ordered 2D array of features and their coefficients
     """
     data_length = x.shape[0]
     chunk = int(data_length / K)
-    min_error = -1
-    best_k = 0
     coefficients = 0
     for i in range(K):
-        x_cv = []  # one chunk size of x
-        y_cv = []
         Xtrain = []
         Ytrain = []
         for j in range(data_length):
-            if int(j / chunk) == i:
-                x_cv.append(x[j])
-                y_cv.append(y[j])
-            else:
+            if int(j / chunk) != i:
                 Xtrain.append(x[j])
                 Ytrain.append(y[j])
-        if func:
-            y_hat = func(x_cv, Xtrain, Ytrain, **kwargs)
-        else:
-            model.fit(Xtrain, Ytrain)
-            coefficients += np.abs(model.coef_)
-            y_hat = model.predict(x_cv)
-        error = calculate_error(y_cv, y_hat)
-        if min_error == -1 or error < min_error:
-            min_error = error
-            best_k = i
-    return best_k, min_error, coefficients
+        model.fit(Xtrain, Ytrain)
+        coefficients += np.abs(model.coef_)
+        dictionary = dict(zip(features, coefficients))
+        sorted_coefs = sorted(dictionary.items(), key=lambda x: x[1])
+    return sorted_coefs
 
 
 if __name__ == '__main__':
@@ -68,15 +44,6 @@ if __name__ == '__main__':
     train_x = np.array(data.drop(['ClaimAmount', 'rowIndex'], 1))
     train_y = np.array(data['ClaimAmount'])
 
-    bk, me, coeffs = k_fold(train_x, train_y, K=10, model=model)
-
-    data = data.drop(['ClaimAmount', 'rowIndex'], 1)
-    dictionary = dict(zip(data.columns.values, coeffs))
-    sorted_d = sorted(dictionary.items(), key=lambda x: x[1])
-
-    print(sorted_d)
-
-
-
-
-
+    coeffs = k_fold_get_coefs(train_x, train_y, K=10,
+                              features=data.drop(['ClaimAmount', 'rowIndex'], 1).columns.values, model=model)
+    print(coeffs)
