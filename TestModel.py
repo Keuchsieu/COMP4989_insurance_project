4d81c80c556dfe398694c7baab4a31b339a84aab
Yuqing Qiu <yuqingqiu93@gmail.com>
2018-11-27 19:15:47 -0800

    Fixed mae namespace issue, better code style

--- TestModel.py
+++ TestModel.py
@@ -14,7 +14,6 @@ import numpy as np
 
 class TestModel:
 
-
     def __init__(self, ohe=(0, 0), features='all', class_feature='all',
                  classify=True, classifier='svc', c_var=1.0, model='Linear',
                  m_alpha=1, poly_p=1, k_fold=10):
@@ -80,7 +79,7 @@ class TestModel:
         if classifier == 'knn':
             self.classifier = KNeighborsClassifier(n_neighbors=c_var)
         elif classifier == 'svc':
-            self.classifier = SVC(C=c_var, kernel='linear')
+            self.classifier = SVC(C=c_var, kernel='rbf')
         elif classifier == 'gnb':
             self.classifier = GaussianNB()
         elif classifier == 'mnb':
@@ -129,12 +128,12 @@ class TestModel:
         errors = []
         scores = []
         for i in range(self.k_fold):
-            x_cv = []
+            x_cv = []  # features of cv on regression
             y_cv = []
-            x_cv_class = []
-            x_train = []
+            x_cv_class = []  # feature of cv on classification
+            x_train = []  # features selected for regression
             y_train = []
-            x_class = []
+            x_class = []  # features selected for classification
             # separate cv set and training set
             for j in range(data_length):
                 if debug:
@@ -166,26 +165,25 @@ class TestModel:
                 y_class_train = []
                 for val in y_train:
                     y_class_train.append(0 if val == 0 else 1)
-                self.classifier.fit(x_train, y_class_train)
+                self.classifier.fit(x_class, y_class_train)
                 y_class_cv = []
                 for val in y_cv:
                     y_class_cv.append(0 if val == 0 else 1)
-                y_hat = self.classifier.predict(x_cv)
+                y_class_pred = self.classifier.predict(x_cv_class)
                 true_pos = 0
                 false_pos = 0
                 false_neg = 0
-                for m in range(0, len(y_hat)):
-                    true_pos += 1 if (y_hat[m] == y_class_cv[m] == 1) else 0
-                    false_pos += 1 if (y_hat[m] == 1 and y_class_cv[m] == 0) else 0
-                    false_neg += 1 if (y_hat[m] == 0 and y_class_cv[m] == 1) else 0
-                if true_pos == 0:
-                    scores.append(0)
-                else:
+                for m in range(0, len(y_class_pred)):
+                    true_pos += 1 if (y_class_pred[m] == y_class_cv[m] == 1) else 0
+                    false_pos += 1 if (y_class_pred[m] == 1 and y_class_cv[m] == 0) else 0
+                    false_neg += 1 if (y_class_pred[m] == 0 and y_class_cv[m] == 1) else 0
+                f1 = 0
+                if true_pos != 0:
                     f1_p = true_pos / (true_pos + false_pos)
                     f1_r = true_pos / (true_pos + false_neg)
                     f1 = 2 * ((f1_p * f1_r) / (f1_p + f1_r))
-                    scores.append(f1)
-                y_hat *= self.classifier.predict(x_cv)
+                scores.append(f1)
+                y_hat *= y_class_pred
             mae = np.mean(np.abs(np.subtract(y_hat, y_cv)))
             errors.append(mae)
         if debug:
@@ -194,6 +192,61 @@ class TestModel:
         kfold_f1 = np.mean(scores)
         self.prediction = kfold_mae
         return kfold_mae, kfold_f1
+
+    def get_f1_only(self, debug=False):
+        data_length = self.x_train.shape[0]
+        chunk = int(data_length / self.k_fold)
+        scores = []
+        for i in range(self.k_fold):
+            x_cv_class = []
+            y_cv = []
+            y_train = []
+            x_class = []
+            for j in range(data_length):
+                if int(j / chunk) == i:
+                    x_cv_class.append(self.x_class[j, :])
+                    y_cv.append(self.y_train[j])
+                else:
+                    x_class.append(self.x_class[j, :])
+                    y_train.append(self.y_train[j])
+            x_cv_class = np.array(x_cv_class)
+            y_cv = np.array(y_cv)
+            x_class = np.array(x_class)
+            y_train = np.array(y_train)
+            if debug:
+                print('Iteration {}'
+                      '\nShape of x_train: {}'
+                      '\nshape of y train: {}'
+                      '\nshape of x cv:  {}'.format(i, x_class.shape, y_train.shape, x_cv_class.shape))
+            y_class_train = []
+            for val in y_train:
+                y_class_train.append(0 if val == 0 else 1)
+            self.classifier.fit(x_class, y_class_train)
+            y_class_cv = []
+            for val in y_cv:
+                y_class_cv.append(0 if val == 0 else 1)
+            y_class_pred = self.classifier.predict(x_cv_class)
+            true_pos = 0
+            false_pos = 0
+            false_neg = 0
+            if debug:
+                print("size of predition {}, size of cv {}".format(len(y_class_pred), len(y_class_cv)))
+            for m in range(0, len(y_class_pred)):
+                true_pos += 1 if (y_class_pred[m] == y_class_cv[m] == 1) else 0
+                false_pos += 1 if (y_class_pred[m] == 1 and y_class_cv[m] == 0) else 0
+                false_neg += 1 if (y_class_pred[m] == 0 and y_class_cv[m] == 1) else 0
+            if debug:
+                print("True pos={}, False pos={}, False neg={}".format(true_pos, false_pos, false_neg))
+            f1 = 0
+            if true_pos != 0:
+                f1_p = true_pos / (true_pos + false_pos)
+                f1_r = true_pos / (true_pos + false_neg)
+                f1 = 2 * ((f1_p * f1_r) / (f1_p + f1_r))
+            scores.append(f1)
+        kfold_f1 = np.mean(scores)
+        if debug:
+            print("F1 score is {}".format(kfold_f1))
+        return kfold_f1
 
 
 if __name__ == '__main__':
@@ -208,13 +261,17 @@ if __name__ == '__main__':
        AND complete the model_completion google sheet to record it
     """
 
-    x = TestModel(features=('feature1', 'feature3', 'feature14'), classify=True, classifier='knn', c_var=1, k_fold=10)
-    error, score = x.get_mae()
-    pred_test = x.predict_test()
-    print("{} with MAE: {}".format(x, error))
-    print("{} with F1: {}".format(x, score))
-
-    from FileWriter import FileWriter
-    print(pred_test.shape)
-    w = FileWriter(file_name=x, data=pred_test)
-    w.write()
+    x = TestModel(class_feature=('feature1','feature2', 'feature3', 'feature5','feature7','feature14','feature16'), classify=True, classifier='knn', c_var=1, k_fold=10)
+    f1ss = x.get_f1_only()
+    print(f1ss)
+    mae, f1ss = x.get_mae()
+    print(f1ss)
+    # error, score = x.get_mae()
+    # pred_test = x.predict_test()
+    # print("{} with MAE: {}".format(x, error))
+    # print("{} with F1: {}".format(x, score))
+    #
+    # from FileWriter import FileWriter
+    # print(pred_test.shape)
+    # w = FileWriter(file_name=x, data=pred_test)
+    # w.write()
